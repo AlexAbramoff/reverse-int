5839c478349c361bb91682cf5f7eedafeb1c24c4
AlexAbramoff <alex93178@gmail.com>
2021-03-05 20:32:39 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  if (n < 0) {
+    n = -n
+  }
+  let str = String(n)
+
+  return +Array.from(str).reverse().join('')
 }
